6707086a08389bdfd6909c796517266bca2761d0
Mark Hording <markhording@gmail.com>
2025-07-03 17:52:39 +0700

    Add authentication, form validation, and metadata to dashboard pages

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,14 @@
+import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { fetchCustomers } from '@/app/lib/data';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
 export default function Page() {
   return <p>Customer Page</p>;
 }
